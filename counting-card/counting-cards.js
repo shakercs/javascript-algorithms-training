f6b2ea44a791226910fde656ba9991cd1218ec11
shakercs <shakercs@gmail.com>
2020-07-20 09:38:38 +0300

    add counting card game

---
+++ counting-card/counting-cards.js
@@ -0,0 +1,43 @@
+var count = 0;
+
+function cc(card) {
+    // Only change code below this line
+
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 7:
+        case 8:
+        case 9:
+            count;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }
+    console.log("Final result", count);
+
+    var holdbet = "Hold";
+    if (count <= 0) {
+        holdbet = "Hold";
+    } else if (count > 0) {
+        holdbet = "Bet";
+    }
+
+    return count + " " + holdbet;
+
+
+    // Only change code above this line
+}
+
+cc(2); cc(3); cc(4); cc(5); cc("J");
+
